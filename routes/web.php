83b1f24325f80973b369897beb26e31e5fbafd61
Yojayro <yojayro.ito@upeu.edu.pe>
2019-09-12 10:55:57 -0500

    Segunda Clase

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('cliente/index',"ClienteController@index");
+Route::get('cliente/show/{id}',"ClienteController@show");
